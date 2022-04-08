facc567a42a84f32c906df18cdbe9e03f38a22df
guan <kuniyasu.guan@gmail.com>
2022-04-08 17:58:56 +0900

    components

---
+++ src/lib-components/components.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+
+import imgix from '@guan/tgl-components/lib-components/imgix.vue'
+import sourcex from '@guan/tgl-components/src/lib-components/sourcex.vue'
+
+Vue.component('imgix', imgix)
+Vue.component('sourcex', sourcex)
